15592501d296e2632faba5dc86b935e77a1d8607
Horizon733 <dishantgandhi733@gmail.com>
2021-10-01 21:24:51 +0530

    initial commit

---
+++ app/src/main/java/com/example/loginkitsample/MainActivity.kt
@@ -0,0 +1,169 @@
+package com.example.loginkitsample
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.Nullable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
+import com.example.loginkitsample.ui.theme.LoginKitSampleTheme
+import com.example.loginkitsample.ui.theme.SnapbtnColor
+import com.snapchat.kit.sdk.SnapLogin
+import com.snapchat.kit.sdk.login.models.UserDataResponse
+import com.snapchat.kit.sdk.login.networking.FetchUserDataCallback
+import dagger.hilt.android.AndroidEntryPoint
+import com.snapchat.kit.sdk.login.models.MeData
+import com.snapchat.kit.sdk.core.controller.LoginStateController
+import com.snapchat.kit.sdk.core.controller.LoginStateController.OnLoginStateChangedListener
+
+
+val username = mutableStateOf("")
+val bitmoji = mutableStateOf("")
+
+class MainActivity : ComponentActivity() {
+    private lateinit var mLoginStateChangedListener: OnLoginStateChangedListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginKitSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    MainScreen()
+                }
+            }
+        }
+        mLoginStateChangedListener = object: OnLoginStateChangedListener{
+            override fun onLoginSucceeded() {
+                getUserDetails(this@MainActivity)
+            }
+
+            override fun onLoginFailed() {
+                Toast.makeText(this@MainActivity, "Login Failed", Toast.LENGTH_LONG).show()
+
+            }
+
+            override fun onLogout() {
+                Toast.makeText(this@MainActivity, "Logged out!", Toast.LENGTH_LONG).show()
+            }
+
+        }
+        SnapLogin.getLoginStateController(this).addOnLoginStateChangedListener(mLoginStateChangedListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        SnapLogin.getAuthTokenManager(this).clearToken()
+    }
+}
+
+
+
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+    val is_logged_in = SnapLogin.isUserLoggedIn(context)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = rememberImagePainter(
+                bitmoji.value,
+                builder = {
+                    transformations(CircleCropTransformation())
+                }
+            ),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+                .border(width = 1.dp, color = MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = username.value,
+            onValueChange = { username.value = it },
+            placeholder = { Text("Display Name", color = Color.LightGray) }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                SnapLogin.getAuthTokenManager(context).startTokenGrant()
+            },
+            shape = MaterialTheme.shapes.large,
+            colors = ButtonDefaults.buttonColors(backgroundColor = SnapbtnColor),
+            enabled = true
+
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_snapchat),
+                contentDescription = "snapchat icon",
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+                tint = Color.White
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Login with Snapchat")
+
+        }
+    }
+}
+
+
+fun getUserDetails(context: Context){
+    val query = "{me{bitmoji{avatar},displayName}}"
+    val is_logged_in = SnapLogin.isUserLoggedIn(context)
+    Log.e("Is user logged in", "$is_logged_in")
+    SnapLogin.fetchUserData(context, query, null, object : FetchUserDataCallback{
+        override fun onSuccess(userDataResponse: UserDataResponse?) {
+            if (userDataResponse != null) {
+                val data = userDataResponse.data.me
+                if (data != null) {
+                    username.value = data.displayName
+                    bitmoji.value  = data.bitmojiData.avatar
+                    Log.e("data", "Name: ${data.displayName}, avatar: ${data.bitmojiData.avatar}")
+                }
+            }
+        }
+
+        override fun onFailure(isNetworkError: Boolean, statusCode: Int) {
+            Log.e("Snapkit Failure", "network error: $isNetworkError, status code: $statusCode")
+        }
+
+
+    })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginKitSampleTheme {
+        MainScreen()
+    }
+}
